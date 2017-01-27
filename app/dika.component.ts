2c7eefa78470b3e401a5f4341a06225c54cca8af
Andika Nugraha <m.andika.nugraha@gmail.com>
2017-01-27 22:47:13 +0700

    Component and data binding

--- app/dika.component.ts
+++ app/dika.component.ts
@@ -2,9 +2,26 @@ import {Component} from '@angular/core';
 
 @Component({
 	selector	: 'andika',
-	template	:'<h3>Andika Nugraha</h3>',
+	template	:`
+					<h3>{{tag}}</h3><h1>{{nama}}</h1>
+					<button (click)="diClick()">Click</button><br/>
+					<img [src]="gambar">
+					<img bind-src="gambar">
+
+				`
 })
 
-export class DikaComponent {
+export class DikaComponent{
+	
+	private tag, gambar;
+	nama = 'Andika Nugraha';
+	constructor(){
+		this.tag = 'Hello World';
+		this.gambar = 'https://d2lm6fxwu08ot6.cloudfront.net/img-thumbs/960w/W0RUS6FWBJ.jpg'
+	}
+
+	public diClick(){
+		console.log('Di Click...');
+	}
 
 }
