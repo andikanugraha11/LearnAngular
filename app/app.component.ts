dcbc9812eda755cd6b793adbe9945151f1054f8a
AndikaNugraha11 <m.andika.nugraha@gmail.com>
2017-01-29 17:42:24 +0700

    Pipe

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, NgModule} from '@angular/core';
+import { Component, Pipe, PipeTransform } from '@angular/core';
 
 
 @Component({
@@ -9,13 +9,22 @@ import { Component, NgModule} from '@angular/core';
   				<span *ngSwitchCase="'dika'" >Muahmmad Andika</span>
   				<span *ngSwitchDefault>Nama salah</span>
   			</span>
-
   			<ul>
   				<li *ngFor="let receh of uang">
   				{{receh}}
   				</li>
   			</ul>
-  			`
+        <h1>{{buku}}</h1>
+        <h1>{{buku | uppercase}}</h1>
+        <h1>{{buku | lowercase}}</h1>
+        <h1>{{tanggal | date:'shortDate' }}</h1>
+        <h1>{{tanggal | date:'longDate' }}</h1>
+        <h1>{{harga | currency:'IDR':'true' }}</h1>
+        <andika *ngIf="false"></andika>
+        <hr>
+        <input type="text" [(ngModel)]="filterSkill">
+        <p>{{5 | exponentialStrength: 10}}</p>
+        `
 })
 
 
@@ -25,10 +34,18 @@ export class AppComponent  {
 	private tampil;
 	private nama;
 	private uang;
+  private buku;
+  private tanggal;
+  private harga;
+  private skill;
 
 	constructor(){
 		this.tampil = true;
 		this.nama = "ika";
+    this.skill = ['Angular','CodeIgniter','PHP','ReactJS','NodeJS'];
 		this.uang = ['1000','2000','5000','10000','20000'];
+    this.buku = "Head First JavaScript";
+    this.tanggal = new Date(2017, 0, 29);
+    this.harga = 2000;
 	}
 }
--- app/app.module.ts
+++ app/app.module.ts
@@ -2,11 +2,12 @@ import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule }   from '@angular/forms';
 import { DikaComponent } from './dika.component'; //tambahan
+import { ExponentialStrengthPipe } from './dika.pipe';
 import { AppComponent }  from './app.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ], //tambahan
-  declarations: [ AppComponent, DikaComponent], //tambahan
-  bootstrap:    [ AppComponent ]
+  declarations: [ AppComponent, DikaComponent, ExponentialStrengthPipe], //tambahan
+  bootstrap:    [ AppComponent ],
 })
 export class AppModule { }
